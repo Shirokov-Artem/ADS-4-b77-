08192c9cfdae919e89c1811396c63c398d26ea84
Артем Широков <125153836+Shirokov-Artem@users.noreply.github.com>
2023-03-17 20:39:38 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,7 +1,7 @@
 // Copyright 2021 NNTU-CS
 void Sort(int* arr, int len) {
     int t;
-    for (int i = 0; i < len - 1 ; i++) {
+    for (int i = 0; i < len; i++) {
         for (int j = 0; j < len - i - 1; j++) {
             if (arr[j + 1] < arr[j]) {
                 t = arr[j];
@@ -12,10 +12,9 @@ void Sort(int* arr, int len) {
     }
 }
 int countPairs1(int *arr, int len, int value) {
-    Sort(arr, len);
     int countSum = 0;
     int newLen = len - 1;
-    for (int i = 0; i < newLen; i++) {
+    for (int i = 0; i < len - 1; i++) {
         for (int j = i + 1; j < len; j++) {
             if (arr[i] + arr[j] == value) {
                 countSum++;
