92cd57c2542aa4df26f8d4fba177d2e21490a6e2
Артем Широков <125153836+Shirokov-Artem@users.noreply.github.com>
2023-03-18 16:57:04 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -11,21 +11,15 @@ int countPairs1(int *arr, int len, int value) {
     return countSum;
 }
 int countPairs2(int *arr, int len, int value) {
-    int count = 0;
-    int i = 0;
-    int j = len - 1;
-    while (i < j) {
-        if (arr[i] + arr[j] == value) {
-            count++;
-            i++;
-            j--;
-        } else if (arr[i] + arr[j] < value) {
-            i++;
-        } else {
-            j--;
+    int countSum = 0;
+    for (int i = 0; i < len - 1; i++) {
+        for (int j = len - 1; i < j; j--) {
+            if (arr[i] + arr[j] == value) {
+                countSum++;
+            }
         }
     }
-    return count;
+    return countSum;
 }
 int countPairs3(int *arr, int len, int value) {
     int count = 0;
