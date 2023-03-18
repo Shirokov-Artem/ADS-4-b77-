cf9e0c5c3b481f1c332fecfd541885c79ecbfe26
Артем Широков <125153836+Shirokov-Artem@users.noreply.github.com>
2023-03-18 16:50:57 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,7 +1,7 @@
 // Copyright 2021 NNTU-CS
 int countPairs1(int *arr, int len, int value) {
     int countSum = 0;
-    for (int i = 0; i < len; i++) {
+    for (int i = 0; i < len - 1; i++) {
         for (int j = i + 1; j < len; j++) {
             if (arr[i] + arr[j] == value) {
                 countSum++;
