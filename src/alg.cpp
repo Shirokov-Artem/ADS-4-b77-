8b9c379e6e42ef9a3d90b2868cf723ece2d74d14
Артем Широков <125153836+Shirokov-Artem@users.noreply.github.com>
2023-03-18 16:45:41 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -27,26 +27,19 @@ int countPairs2(int *arr, int len, int value) {
     }
     return count;
 }
-int binary_search(int* arr, int len, int value) {
-    int left = 0, right = len - 1;
-    while (left <= right) {
-        int mid = left + (right - left) / 2;
-        if (arr[mid] == value) {
-            return mid;
-        } else if (arr[mid] < value) {
-            left = mid + 1;
-        } else {
-            right = mid - 1;
-        }
-    }
-    return -1;
-}
 int countPairs3(int *arr, int len, int value) {
     int count = 0;
-    for (int i = 0; i < len - 1; i++) {
-        int j = binary_search(arr, len, value - arr[i]);
-        if (j != -1 && j > i) {
+    int i = 0;
+    int j = len - 1;
+    while (i < j) {
+        if (arr[i] + arr[j] == value) {
             count++;
+            i++;
+            j--;
+        } else if (arr[i] + arr[j] < value) {
+            i++;
+        } else {
+            j--;
         }
     }
     return count;
